6638281a8764f788e7b80ce69ecbe7cf5b9744fc
tiananugerah <32483356+tiananugerah@users.noreply.github.com>
2020-01-10 16:10:02 +0700

    update

--- gaji.sql
+++ gaji.sql
@@ -1,98 +1,177 @@
-/*
-SQLyog Community v12.4.3 (64 bit)
-MySQL - 5.6.12-log : Database - gaji_karyawan
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 10, 2020 at 10:08 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.33
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-/*Table structure for table `gaji` */
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-DROP TABLE IF EXISTS `gaji`;
+--
+-- Database: `gaji`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gaji`
+--
 
 CREATE TABLE `gaji` (
-  `id_gaji` int(11) NOT NULL AUTO_INCREMENT,
+  `id_gaji` int(11) NOT NULL,
   `tgl` date DEFAULT NULL,
   `nik` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id_gaji`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `konfirmasi` int(1) NOT NULL,
+  `kirim` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `gaji` */
+--
+-- Dumping data for table `gaji`
+--
 
-insert  into `gaji`(`id_gaji`,`tgl`,`nik`) values 
-(4,'2017-11-10','68657537683');
+INSERT INTO `gaji` (`id_gaji`, `tgl`, `nik`, `konfirmasi`, `kirim`) VALUES
+(4, '2017-11-10', '68657537683', 0, 0),
+(6, '2020-01-03', '68657537683', 0, 0);
 
-/*Table structure for table `karyawan` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `karyawan`;
+--
+-- Table structure for table `karyawan`
+--
 
 CREATE TABLE `karyawan` (
-  `id_karyawan` int(11) NOT NULL AUTO_INCREMENT,
+  `id_karyawan` int(11) NOT NULL,
   `nik` varchar(20) DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `nama` varchar(100) DEFAULT NULL,
-  `alamat` text,
+  `alamat` text DEFAULT NULL,
   `jenis_kelamin` varchar(10) DEFAULT NULL,
   `agama` varchar(20) DEFAULT NULL,
   `pendidikan` varchar(50) DEFAULT NULL,
   `asal_sekolah` varchar(100) DEFAULT NULL,
-  `id_pekerjaan` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_karyawan`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `id_pekerjaan` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `karyawan` */
+--
+-- Dumping data for table `karyawan`
+--
 
-insert  into `karyawan`(`id_karyawan`,`nik`,`username`,`password`,`nama`,`alamat`,`jenis_kelamin`,`agama`,`pendidikan`,`asal_sekolah`,`id_pekerjaan`) values 
-(1,'68657537683','user','user','Joni Saputra','jambi','Laki-laki','Islam','','',1);
+INSERT INTO `karyawan` (`id_karyawan`, `nik`, `username`, `password`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `pendidikan`, `asal_sekolah`, `id_pekerjaan`) VALUES
+(1, '68657537683', 'user', 'user', 'Joni Saputra', 'jambi', 'Laki-laki', 'Islam', '', '', 1);
 
-/*Table structure for table `pekerjaan` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `pekerjaan`;
+--
+-- Table structure for table `pekerjaan`
+--
 
 CREATE TABLE `pekerjaan` (
-  `id_pekerjaan` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pekerjaan` int(11) NOT NULL,
   `pekerjaan` varchar(30) DEFAULT NULL,
   `gapok` int(11) DEFAULT NULL,
   `tukes` int(11) DEFAULT NULL,
   `tutra` int(11) DEFAULT NULL,
   `tupen` int(11) DEFAULT NULL,
-  `tukel` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_pekerjaan`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `tukel` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `pekerjaan` */
+--
+-- Dumping data for table `pekerjaan`
+--
 
-insert  into `pekerjaan`(`id_pekerjaan`,`pekerjaan`,`gapok`,`tukes`,`tutra`,`tupen`,`tukel`) values 
-(1,'Supervisor',1800000,125000,50000,0,NULL),
-(2,'Karyawan Tetap',1500000,125000,100000,50000,NULL);
+INSERT INTO `pekerjaan` (`id_pekerjaan`, `pekerjaan`, `gapok`, `tukes`, `tutra`, `tupen`, `tukel`) VALUES
+(1, 'Supervisor', 1800000, 125000, 50000, 0, NULL),
+(2, 'Karyawan Tetap', 1500000, 125000, 100000, 50000, NULL);
 
-/*Table structure for table `user` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `user`;
+--
+-- Table structure for table `user`
+--
 
 CREATE TABLE `user` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
   `nama` varchar(50) DEFAULT NULL,
   `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `user` */
+--
+-- Dumping data for table `user`
+--
 
-insert  into `user`(`id_user`,`nama`,`username`,`password`) values 
-(1,'Administrator','admin','admin');
+INSERT INTO `user` (`id_user`, `nama`, `username`, `password`) VALUES
+(1, 'Administrator', 'admin', 'admin');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gaji`
+--
+ALTER TABLE `gaji`
+  ADD PRIMARY KEY (`id_gaji`);
+
+--
+-- Indexes for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indexes for table `pekerjaan`
+--
+ALTER TABLE `pekerjaan`
+  ADD PRIMARY KEY (`id_pekerjaan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gaji`
+--
+ALTER TABLE `gaji`
+  MODIFY `id_gaji` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pekerjaan`
+--
+ALTER TABLE `pekerjaan`
+  MODIFY `id_pekerjaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
